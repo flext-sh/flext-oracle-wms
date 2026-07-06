48ac40f45f84314aed4b1eb028231bd596a20118
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 00:21:32 -0300

    fixes

--- src/flext_oracle_wms/_utilities/__init__.py
+++ src/flext_oracle_wms/_utilities/__init__.py
@@ -8,20 +8,12 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_oracle_wms._utilities.auth import (
-        FlextOracleWmsUtilitiesAuth as FlextOracleWmsUtilitiesAuth,
-    )
-    from flext_oracle_wms._utilities.client import (
-        FlextOracleWmsUtilitiesClient as FlextOracleWmsUtilitiesClient,
-    )
-    from flext_oracle_wms._utilities.discovery import (
-        FlextOracleWmsUtilitiesDiscovery as FlextOracleWmsUtilitiesDiscovery,
-    )
-    from flext_oracle_wms._utilities.filtering import (
-        FlextOracleWmsUtilitiesFiltering as FlextOracleWmsUtilitiesFiltering,
-    )
+    from flext_oracle_wms._utilities.auth import FlextOracleWmsUtilitiesAuth
+    from flext_oracle_wms._utilities.client import FlextOracleWmsUtilitiesClient
+    from flext_oracle_wms._utilities.discovery import FlextOracleWmsUtilitiesDiscovery
+    from flext_oracle_wms._utilities.filtering import FlextOracleWmsUtilitiesFiltering
     from flext_oracle_wms._utilities.http_client import (
-        FlextOracleWmsUtilitiesHttpClient as FlextOracleWmsUtilitiesHttpClient,
+        FlextOracleWmsUtilitiesHttpClient,
     )
 _LAZY_IMPORTS = build_lazy_import_map(
     {
