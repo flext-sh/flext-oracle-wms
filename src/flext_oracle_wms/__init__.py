804e2f9608a6568276c136bad8ed69ec3522aa60
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-05 21:28:00 -0300

    fixes

--- src/flext_oracle_wms/__init__.py
+++ src/flext_oracle_wms/__init__.py
@@ -19,14 +19,7 @@ from flext_oracle_wms.__version__ import (
 from flext_oracle_wms._exports import FLEXT_ORACLE_WMS_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_core._root_typing_parts import (
-        d as d,
-        e as e,
-        h as h,
-        r as r,
-        s as s,
-        x as x,
-    )
+    from flext_core import d as d, e as e, h as h, r as r, s as s, x as x
     from flext_oracle_wms.api import (
         FlextOracleWmsApi as FlextOracleWmsApi,
         oracle_wms as oracle_wms,
