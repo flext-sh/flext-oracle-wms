28ec072241ab418f5520cbf59be1b25b1dbba069
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-08 13:10:27 -0300

    fixes

--- examples/__init__.py
+++ examples/__init__.py
@@ -11,7 +11,7 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 if TYPE_CHECKING:
     from flext_oracle_wms import c, d, e, h, m, p, r, s, t, u, x
 
-    from . import tests as tests
+    from . import tests
 __all__: tuple[str, ...] = (
     "c",
     "d",
--- tests/__init__.py
+++ tests/__init__.py
@@ -11,7 +11,7 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 if TYPE_CHECKING:
     from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
 
-    from . import unit as unit
+    from . import unit
     from .base import (
         TestsFlextOracleWmsServiceBase,
         TestsFlextOracleWmsServiceBase as s,
